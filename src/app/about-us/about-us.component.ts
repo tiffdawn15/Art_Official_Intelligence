b0412c065d69edd642d25b3504260653dbce99da
Tiffany Messer <tiffanymesser15@gmail.com>
2024-05-12 18:12:10 -0400

    Implamented builder.io landing page. Next step About us page

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { BuilderBlock } from '@builder.io/angular'; // <-- import BuilderBlock
 
 @Component({
   selector: 'app-about-us',
-  templateUrl: './about-us.component.html',
+  template: 'Hello: {{name}}!',
+
   styleUrls: ['./about-us.component.scss']
 })
 export class AboutUsComponent {
 
+  @Input()
+  text = ''; // <-- name Angular and Builder inputs the same 
+  name = ''
 }
+
+BuilderBlock({
+  tag: 'app-about-us', 
+  name: 'Custom thing',
+  inputs: [
+    {
+      name: 'text', // <-- this name matches the Angular @Input() above
+      type: 'string',
+    },
+  ],
+})(AboutUsComponent);
